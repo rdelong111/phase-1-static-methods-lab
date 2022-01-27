88b48f46ef7a7c77992c1d4e1b5b9a70c8b05f84
RyanDeLong <rdelong111@gmail.com>
2022-01-27 15:01:51 -0600

    commit tree e315a228fb0f8263234117426226d129f9585242

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    let array = string.split(' ');
+    const excepts = {
+      'the': true,
+      'a': true,
+      'an': true,
+      'but': true,
+      'of': true,
+      'and': true,
+      'for': true,
+      'at': true,
+      'by': true,
+      'from': true
+    };
+    for (let i = 0; i < array.length; i++) {
+      if (!(array[i] in excepts) || i === 0) {
+        array[i] = array[i][0].toUpperCase() + array[i].slice(1);
+      }
+    }
+    return array.join(' ');
+  }
 }
